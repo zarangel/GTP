a383942c663fe17a6b32819d98bdf2a155bf8e0e
Victor Angel Zaragoza <zarangel@hotmail.com>
2020-09-15 11:54:30 -0700

    header mod

--- header.php
+++ header.php
@@ -12,11 +12,14 @@
     <div class="container">
         <div class="row">
             <div class="col-xl-12">
-                <nav class="navbar navbar-expand-lg navbar-light">
-                    <a class="navbar-brand" href="#">GTP</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" 
-                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
-                            aria-expanded="false" aria-label="Toggle navigation">
+                <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+                    <a class="navbar-brand" href="<?php echo get_home_url();?> ">GTP</a>
+                    <button class="navbar-toggler" type="button" 
+                            data-toggle="collapse" 
+                            data-target="#navbarSupportedContent" 
+                            aria-controls="navbarSupportedContent" 
+                            aria-expanded="false" 
+                            aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
